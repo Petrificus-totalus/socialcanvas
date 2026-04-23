09603e303dc637b2487f90205d5cdc70b60978ff
Nanhao <1966710331@qq.com>
2026-04-23 23:33:26 +1000

    change

--- src/app/clientwrapper.jsx
+++ src/app/clientwrapper.jsx
@@ -22,7 +22,7 @@ export default function VideoWrapper({ children }) {
         }}
       >
         <video
-          src="/logo.mp4"
+          src="/slogan.mp4"
           autoPlay
           muted
           playsInline
